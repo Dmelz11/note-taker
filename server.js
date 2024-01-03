1c50d482d01f08b599c5bf4e117d8eedfd9204fc
davidmelendez <Dmelz11@github.com>
2024-01-03 15:30:18 -0800

    trying to fix bugs in server.js

--- server.js
+++ server.js
@@ -1,20 +1,18 @@
-const PORT = process.env.PORT ||3001;
+const PORT = 3001;
 const express = require('express');
 const app = express();
-const fs = require('fs');
-const path = require('path');
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-const apiRoutes = require('./routes/apiRoutes');
-app.use('api', apiRoutes);
-const htmlRoutes = require('./routes/htlmRoutes');
-app.use('/', htmlRoutes);
+const api_routes = require('/routes/api-routes');
+app.use(api_routes);
+const html_routes = require('/routes/html-routes');
+app.use(html_routes);
 
 app.listen(PORT,()=>{
-     console.log('Listening on port:${PORT}')
+     console.log('Listening on port:${PORT}');
     });
 
 
