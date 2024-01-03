62080d1be4d127d435273a47ef3cbdc689dbb4ca
davidmelendez <Dmelz11@github.com>
2024-01-03 12:31:12 -0800

    changed some code in server.js

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
 const PORT = process.env.PORT ||3001;
 const express = require('express');
 const app = express();
+const fs = require('fs');
+const path = require('path');
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(express.static('public'));
 
-const apiRoutes = require('/routes/apiRoutes');
-app.use(apiRoutes);
-const htmlRoutes = require('/routes/htlmRoutes');
-app.use(htmlRoutes)
+const apiRoutes = require('./routes/apiRoutes');
+app.use('api', apiRoutes);
+const htmlRoutes = require('./routes/htlmRoutes');
+app.use('/', htmlRoutes);
 
-app.listen(PORT,()=> console.log('Listening on port:${PORT}'));
+app.listen(PORT,()=>{
+     console.log('Listening on port:${PORT}')
+    });
 
 
